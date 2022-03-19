8f4ef954d3e724a3d173bacdf6498301017e4d9c
Darsh2010 <97524332+Darsh2010@users.noreply.github.com>
2022-03-19 13:49:38 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJYeTyinQn0cKxXmoTd7sccA_HAY_42so",
+  authDomain: "project-aebe2.firebaseapp.com",
+  databaseURL: "https://project-aebe2-default-rtdb.firebaseio.com",
+  projectId: "project-aebe2",
+  storageBucket: "project-aebe2.appspot.com",
+  messagingSenderId: "844742745225",
+  appId: "1:844742745225:web:301f70c720f54f11b37a58",
+  measurementId: "G-9RLB4JH6TG"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
